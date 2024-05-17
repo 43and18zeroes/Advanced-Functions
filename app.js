5471b038bf8e019f018b66862870a6fc4693b63e
Christoph <c.wagner.germany@gmail.com>
2024-05-17 18:56:45 +0200

    17May7

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ function add(num1, num2) {
   return num1 + num2;
 }
 
-console.log(add(1, 5));
-
+console.log(add(1, 5)); // 6
 
 // Function Expression Syntax:
-const add2 = function(num1, num2) {
-    return num1 + num2;
-}
+const add2 = function (num1, num2) {
+  return num1 + num2;
+};
 
-console.log(add2(1, 5));
+console.log(add2(1, 5)); // 6
+
+// Arrow Function Expression Syntax:
+const add3 = (num1, num2) => {
+  return num1 + num2;
+};
+
+console.log(add3(1, 5)); // 6
