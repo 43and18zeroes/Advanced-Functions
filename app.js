c64310642ebe8b9b376992aa7ec4c8730b096287
Christoph <c.wagner.germany@gmail.com>
2024-05-17 19:16:54 +0200

    17May9

--- app.js
+++ app.js
@@ -24,3 +24,10 @@ const add4 = (num1, num2) => {
 };
 
 console.log(add4(1, 5)); // 6
+
+// Impure because we cannot predict the outcome
+function addRandom(num1) {
+    return num1 + Math.random();
+}
+
+console.log(addRandom(5));
