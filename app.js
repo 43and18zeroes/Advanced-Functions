00c852e33322bb2842f552b80ec64d4ca160b868
Christoph <c.wagner.germany@gmail.com>
2024-05-17 18:54:48 +0200

    17May6

---
+++ app.js
@@ -0,0 +1,14 @@
+// Function Declaration Syntax:
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(add(1, 5));
+
+
+// Function Expression Syntax:
+const add2 = function(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(add2(1, 5));
