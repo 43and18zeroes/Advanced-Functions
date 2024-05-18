1d68ee6a1f20de09733b698cdc44f16d2f1fb757
Christoph <c.wagner.germany@gmail.com>
2024-05-18 15:06:06 +0200

    18May6

--- app.js
+++ app.js
@@ -82,3 +82,15 @@ userName = 'Manuel';
 
 greetUser(); // Hi Maximilian
 
+function powerOf(x, n) {
+  let result = 1;
+
+  for (let i = 0; i < n; i++) {
+    // result = result * x;
+    result *= x;
+  }
+
+  return result;
+}
+
+console.log(powerOf(2, 3)); // 8
