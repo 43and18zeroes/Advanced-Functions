62b1be196ad6f2757f83bb5c61989949089e1305
Christoph <c.wagner.germany@gmail.com>
2024-05-18 15:27:57 +0200

    18May9

--- app.js
+++ app.js
@@ -94,10 +94,11 @@ greetUser(); // Hi Maximilian
 // }
 
 function powerOf(x, n) {
-  if (n === 1) {
-    return x;
-  }
-  return x * powerOf(x, n - 1);
+  // if (n === 1) {
+  //   return x;
+  // }
+  // return x * powerOf(x, n - 1);
+  return n === 1 ? x : x * powerOf(x, n - 1);
 }
 
 console.log(powerOf(2, 3)); // 8
